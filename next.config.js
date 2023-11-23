0a6e2495b2552cb5dcd5ec22938e46db47d766ce
Daniel Kamar <kamarster@gmail.com>
2023-11-23 15:14:01 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,29 +1,5 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {}
-// module.exports = nextConfig
+const nextComposePlugin = require('next-compose-plugin');
 
-const withAntdLess = require('next-plugin-antd-less');
+const lessPlugin = require('./less-plugin');
 
-module.exports = withAntdLess({
-//   reactStrictMode: true,
-  modifyVars: {
-    '@primary-color': '#0050b3',
-  },
-  env: {
-    API_HOST: process.env.API_HOST
-  },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ]
-      }
-    ]
-  }
-});
+module.exports = nextComposePlugin([lessPlugin]);
