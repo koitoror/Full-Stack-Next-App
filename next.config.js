caa30edcf6d8099fc456122f8aa31a1db1ea5d8d
Daniel Kamar <kamarster@gmail.com>
2023-11-23 12:24:00 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,3 +2,29 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+const withAntdLess = require('next-plugin-antd-less');
+
+module.exports = withAntdLess({
+  reactStrictMode: true,
+  modifyVars: {
+    '@primary-color': '#0050b3',
+  },
+  env: {
+    API_HOST: process.env.API_HOST
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ]
+      }
+    ]
+  }
+});
