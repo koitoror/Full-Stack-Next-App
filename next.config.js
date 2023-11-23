4299bc44f469c0ea17de752b991c35e291f8cf0e
Daniel Kamar <kamarster@gmail.com>
2023-11-23 13:20:27 +0300

    Update next.config.ts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
+// module.exports = nextConfig
 
 const withAntdLess = require('next-plugin-antd-less');
 
