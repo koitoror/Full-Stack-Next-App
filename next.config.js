28d6a831d78973f858a24b94e21c71a9d04f4dc8
Daniel Kamar <kamarster@gmail.com>
2023-11-23 14:39:23 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@
 const withAntdLess = require('next-plugin-antd-less');
 
 module.exports = withAntdLess({
-  reactStrictMode: true,
+//   reactStrictMode: true,
   modifyVars: {
     '@primary-color': '#0050b3',
   },
