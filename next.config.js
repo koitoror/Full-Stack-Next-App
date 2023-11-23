e3a3099f218200d3dd09b2aa3af57e15f799fabd
Daniel Kamar <kamarster@gmail.com>
2023-11-23 21:44:34 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,20 @@
 const withLess = require('next-with-less');
-const path = require("path");
+const path = require('path');
 
-const pathToLessFileWithVariables = path.resolve(
-  "./styles/variables.less"
-);
+const pathToLessFileWithVariables = path.resolve('./styles/variables.less');
 
 module.exports = withLess({
   // Optional: customize Less loader options
   lessLoaderOptions: {
     javascriptEnabled: true,
     lessOptions: {
-        /* ... */
-        modifyVars: {
+      modifyVars: {
         //   "primary-color": "#9900FF",
-          '@primary-color': '#0050b3',
-          "border-radius-base": "2px",
-          /* ... */
-        },
-      additionalData: (content) =>
+        '@primary-color': '#0050b3',
+        'border-radius-base': '2px',
+      },
+    },
+    additionalData: content =>
       `${content}\n\n@import '${pathToLessFileWithVariables}';`,
   },
 });
