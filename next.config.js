172bcd4f78f11f3741b0fe3ba191612fcf9974a1
Daniel Kamar <kamarster@gmail.com>
2023-11-23 21:25:07 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,50 +1,22 @@
-/* eslint-disable */
-const withLess = require('@zeit/next-less');
-const withSass = require('@zeit/next-sass');
-const lessToJS = require('less-vars-to-js');
-const fs = require('fs');
-const path = require('path');
+const withLess = require('next-with-less');
+const path = require("path");
 
-// Where your antd-custom.less file lives
-const themeVariables = lessToJS(
-  fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8')
+const pathToLessFileWithVariables = path.resolve(
+  "your-file-with-antd-variables.less"
 );
 
-module.exports = withSass({
-  cssModules: true,
-  ...withLess({
-    lessLoaderOptions: {
-      javascriptEnabled: true,
-      modifyVars: themeVariables, // make your antd custom effective
-      importLoaders: 0
-    },
-    cssLoaderOptions: {
-      importLoaders: 3,
-      localIdentName: '[local]___[hash:base64:5]'
-    },
-    webpack: (config, { isServer }) => {
-      //Make Ant styles work with less
-      if (isServer) {
-        const antStyles = /antd\/.*?\/style.*?/;
-        const origExternals = [...config.externals];
-        config.externals = [
-          (context, request, callback) => {
-            if (request.match(antStyles)) return callback();
-            if (typeof origExternals[0] === 'function') {
-              origExternals[0](context, request, callback);
-            } else {
-              callback();
-            }
-          },
-          ...(typeof origExternals[0] === 'function' ? [] : origExternals)
-        ];
+module.exports = withLess({
+  // Optional: customize Less loader options
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+    modifyVars: {
+        // '@primary-color': '#0050b3',
+        "primary-color": "#9900FF",
+        "border-radius-base": "2px",
 
-        config.module.rules.unshift({
-          test: antStyles,
-          use: 'null-loader'
-        });
-      }
-      return config;
-    }
-  })
+        /* ... */
+      },
+      additionalData: (content) =>
+      `${content}\n\n@import '${pathToLessFileWithVariables}';`,
+  },
 });
