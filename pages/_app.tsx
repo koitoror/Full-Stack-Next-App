1438ba5d459b689cc2e1d0a33914305f5dfdcb98
Daniel Kamar <kamarster@gmail.com>
2023-11-25 21:17:17 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,13 @@ import { PersistGate } from 'redux-persist/integration/react';
 import store, { persistor } from '../store';
 import '../firebase';
 
-const MyApp = ({ Component, pageProps }) => {
+// Explicitly specify the types for Component and pageProps
+interface AppProps {
+  Component: React.ComponentType<any>;
+  pageProps: any;
+}
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
