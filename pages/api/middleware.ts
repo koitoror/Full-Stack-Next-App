7a23b1c328572c88aae0970fbbaee44c49b6a770
Daniel Kamar <kamarster@gmail.com>
2023-11-25 21:22:14 +0300

    Update middleware.ts

--- pages/api/middleware.ts
+++ pages/api/middleware.ts
@@ -1,19 +1,31 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+// Exporting Next.js API types for better type inference in your code
 export type { NextApiRequest, NextApiResponse } from 'next';
 
+// Define the type for the middleware function
+type MiddlewareFunction = (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  callback: (result: any) => void
+) => void;
+
+// A utility function for running middlewares in Next.js API routes
 export const runMiddleware = (
   req: NextApiRequest,
   res: NextApiResponse,
-  fn
+  fn: MiddlewareFunction
 ) => {
   return new Promise((resolve, reject) => {
+    // Execute the provided middleware function (fn) with the request, response, and a callback
     fn(req, res, (result) => {
+      // If the result is an instance of Error, reject the promise with the error
       if (result instanceof Error) {
-        return reject(result)
+        return reject(result);
       }
 
-      return resolve(result)
-    })
-  })
+      // If the result is not an error, resolve the promise with the result
+      return resolve(result);
+    });
+  });
 };
