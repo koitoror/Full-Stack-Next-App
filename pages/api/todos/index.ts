4f896ffc9915c02e2ac858c7f7285cb52d659ba6
Daniel Kamar <kamarster@gmail.com>
2023-11-27 14:56:43 +0300

    Update index.ts

--- pages/api/todos/index.ts
+++ pages/api/todos/index.ts
@@ -56,8 +56,8 @@ const getTodos = async (): Promise<GetTodosResult> => {
 const addTodo = async (todo: Todo): Promise<AddTodoResult> => {
   try {
     // const docRef = await addDoc(collection(db, collectionName), todo);
-    const docRef = await addDoc(collection(db as Firestore, collectionName) as CollectionReference<DocumentData, DocumentData>, todo);
-    // const docRef = await addDoc(collection(db as Firestore, collectionName), todo);
+    // const docRef = await addDoc(collection(db as Firestore, collectionName) as CollectionReference<DocumentData, DocumentData>, todo);
+    const docRef = await addDoc(collection(db as Firestore, collectionName), todo);
 
 
     const modifyTodo = { id: docRef.id, ...todo };
