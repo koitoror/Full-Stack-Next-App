81d839555d9c4ee93feed2a0c43fc7b393a18f27
Daniel Kamar <kamarster@gmail.com>
2023-11-27 16:00:22 +0300

    Update sign-in.ts

--- pages/api/users/sign-in.ts
+++ pages/api/users/sign-in.ts
@@ -25,25 +25,28 @@ const signIn = async (email: string, password: string): Promise<SignInResult> =>
     const response = await signInWithEmailAndPassword(auth, email, password);
     return { user: response.user, status: StatusCode.OK };
   } catch (_error) {
+    if (isAuthError(_error)) {
+      const error: AuthError = _error;
+      let status = StatusCode.BAD_REQUEST;
 
-      if (isAuthError(_error)) {
-        const error: AuthError = _error;
-        let status = StatusCode.BAD_REQUEST;
-
-        if (error.code === 'auth/user-not-found') {
-          status = StatusCode.NOT_FOUND;
-        }
-        if (error.code === 'auth/wrong-password') {
-          status = StatusCode.FORBIDDEN;
-
-        return { error, status };
-      } else {
-        // If it doesn't match the expected structure, handle it or rethrow
-        throw _error;
+      if (error.code === 'auth/user-not-found') {
+        status = StatusCode.NOT_FOUND;
       }
+      if (error.code === 'auth/wrong-password') {
+        status = StatusCode.FORBIDDEN;
+      }
+
+      return { error, status };
+    } else {
+      // If it doesn't match the expected structure, handle it or rethrow
+      throw _error;
     }
-  };
+  }
+
+  // Add a default return statement
+  return { error: { message: 'Unexpected error' }, status: StatusCode.INTERNAL_SERVER_ERROR };
 };
+
 
 const handler = async (
   req: NextApiRequest,
