8445b547d12ca4ad56b3a448501966db136f2a3b
Daniel Kamar <kamarster@gmail.com>
2023-11-27 16:20:43 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,19 @@ import Head from 'next/head';
 import { Row, Col, Tabs, Spin, message } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { getTodos } from '../store/slices/todoSlice';
-import { RootState } from '../store';
+import { RootState, AppDispatch } from '../store';
 import TodoList from '../components/todos/TodosList';
 import AddTodo from '../components/todos/AddTodo';
 
 const Home: React.FC = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
+
 
   useEffect(() => {
     let callbackFail: (message: string) => void;
     callbackFail = (errorMessage) => message.error(errorMessage);
+    
+    // Dispatch the asynchronous action
     dispatch(getTodos({ callbackFail }));
   }, [dispatch]);
 
@@ -30,24 +33,21 @@ const Home: React.FC = () => {
           sm={{ span: 20, offset: 2 }}
           xs={{ span: 22, offset: 1 }}
         >
-          {isLoading
-            ? (
-                <Spin
-                  size="large"
-                  style={{ display: 'flex', justifyContent: 'center', marginTop: '25vh' }}
-                />
-              )
-            : (
-                <Tabs defaultActiveKey="1">
-                  <Tabs.TabPane tab="Show task list" key="1">
-                    <TodoList />
-                  </Tabs.TabPane>
-                  <Tabs.TabPane tab="Add a task" key="2">
-                    <AddTodo />
-                  </Tabs.TabPane>
-                </Tabs>
-              )
-          }
+          {isLoading ? (
+            <Spin
+              size="large"
+              style={{ display: 'flex', justifyContent: 'center', marginTop: '25vh' }}
+            />
+          ) : (
+            <Tabs defaultActiveKey="1">
+              <Tabs.TabPane tab="Show task list" key="1">
+                <TodoList />
+              </Tabs.TabPane>
+              <Tabs.TabPane tab="Add a task" key="2">
+                <AddTodo />
+              </Tabs.TabPane>
+            </Tabs>
+          )}
         </Col>
       </Row>
     </>
