b1dea21b4cd0930af4814cb557ed3cc4bfe4b69c
Daniel Kamar <kamarster@gmail.com>
2023-11-28 21:21:41 +0300

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,7 @@
 export async function getServerSideProps() {
   return {
     redirect: {
-      destination: 'https://github.com/sk1t0n',
+      destination: 'https://github.com/koitoror',
       permanent: false
     }
   };
