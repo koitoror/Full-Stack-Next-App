e58d75551a83fc0d60e10716d7b38c7c62d9924f
Daniel Kamar <kamarster@gmail.com>
2023-11-28 21:57:13 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+
+'use client';
 import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Analytics />
+        <Analytics mode={'production'} />;
       </body>
     </html>
   )
