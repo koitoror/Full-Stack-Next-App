281e8d4625f0826a6ea0cb06794013c48b2c22dc
Daniel Kamar <daniel@koitoror.ml>
2023-12-17 21:18:07 +0000

    fix(tests): jest configs run

--- __tests__/components/navbar/Menu.test.tsx
+++ __tests__/components/navbar/Menu.test.tsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from "@testing-library/react";
 import Menu from '../../../components/navbar/menu';
 import { MenuItems } from '../../../components/navbar/menu'; 
 
@@ -8,16 +9,32 @@ describe('Menu', () => {
     const mode = 'horizontal';
     const onClick = jest.fn();
     const selectedKeys = ['home'];
+    // const items: MenuItems = {
+    //   leftMenu: [
+    //     { key: 'home', text: 'Home', path: '/' },
+    //     { key: 'about', text: 'About', path: '/about' }
+    //   ],
+    //   rightMenu: [
+    //     { key: 'register', text: 'Register', path: '/register' },
+    //     { key: 'sign-in', text: 'Sign In', path: '/sign-in' }
+    //   ]
+    // };
+
     const items: MenuItems = {
       leftMenu: [
-        { key: 'home', text: 'Home', path: '/' },
-        { key: 'about', text: 'About', path: '/about' }
-      ],
-      rightMenu: [
-        { key: 'register', text: 'Register', path: '/register' },
-        { key: 'sign-in', text: 'Sign In', path: '/sign-in' }
-      ]
+            { key: 'home', text: 'Home', path: '/' },
+            { key: 'about', text: 'About', path: '/about' }
+          ],
+          rightMenu: [
+            { key: 'register', text: 'Register', path: '/register' },
+            { key: 'sign-in', text: 'Sign In', path: '/sign-in' }
+          ],
+      length: 2, // Update length based on menu items
+      pop: () => ({ key: 'removed', text: 'Removed', path: '' }), // Implement or mock function
+      push: (menuItem) => { /* Implement or mock function */ return 3; }, // Update return value
+      concat: (menuItems) => ({ ...items, leftMenu: [...items.leftMenu, ...menuItems] }), // Implement or mock function
     };
+
     const { asFragment } = render(
       <Menu
         theme={theme}
