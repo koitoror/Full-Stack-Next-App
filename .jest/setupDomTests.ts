04032df0e0ef6145e12adf37c75e11293b9e90ff
Daniel Kamar <daniel@koitoror.ml>
2023-12-17 21:56:42 +0000

    fix(tests): TypeError: window.matchMedia workaround is to modify the polyfill

---
+++ .jest/setupDomTests.ts
@@ -0,0 +1,16 @@
+// Dummy import so that the linter won't complain
+import { render } from '@testing-library/react'
+ 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => {},
+  })
+});
